1696f5286c5aca961779bbb6205496032f0bc3b4
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-20 13:13:01 -0800

    second part of task three

--- pull_practice.rb
+++ pull_practice.rb
@@ -29,6 +29,7 @@ def train_noise
   puts "choo choo"
 end
 
+
 # Task 4: Merge Conflict
 # Both partners should modify the same line in different ways.
 # Git doesn't know what to do in this case, so you'll have to
